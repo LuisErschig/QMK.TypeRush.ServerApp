b52e72609803552c6133ae71c4f3f13a5d0594b3
LuisErschig <erschig.luis1@web.de>
2024-10-03 16:55:15 +0200

    Leaderboard implementieren

---
+++ QMK.TypeRush.ServerApp/DataObjects/LeaderboardEntries.cs
@@ -0,0 +1,12 @@
+﻿namespace QMK.TypeRush.ServerApp.DataObjects;
+
+public class LeaderboardEntries
+{
+    public string Name { get; set; } = null!;
+
+    public string? Class { get; set; }
+
+    public double Time { get; set; }
+
+    public int Errors { get; set; }
+}
