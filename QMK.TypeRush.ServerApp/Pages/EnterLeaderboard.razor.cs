be412956755ccadc53667021cd63e36724017a68
LuisErschig <erschig.luis1@web.de>
2024-10-15 09:50:24 +0200

    Fehlerhaftes Parameterhandling in EnterLeaderboard

--- QMK.TypeRush.ServerApp/Pages/EnterLeaderboard.razor.cs
+++ QMK.TypeRush.ServerApp/Pages/EnterLeaderboard.razor.cs
@@ -10,7 +10,18 @@ public partial class EnterLeaderboard
 
     private string activeLeaderboardPath = string.Empty;
 
-    protected override void OnInitialized()
+    private bool parameterAreIncomplete;
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync(); 
+        
+        GetParametersFromUri();
+
+        await GetCurrentLeaderboardPath();
+    }
+
+    private void GetParametersFromUri()
     {
         var uri = this.NavigationManager.ToAbsoluteUri(this.NavigationManager.Uri);
         var queryParams = Microsoft.AspNetCore.WebUtilities.QueryHelpers.ParseQuery(uri.Query);
@@ -19,28 +30,22 @@ public partial class EnterLeaderboard
         {
             this.leaderboardEntries.Errors = Convert.ToInt32(errors);
         }
+        else
+        {
+            this.parameterAreIncomplete = true;
+        }
 
         if (queryParams.TryGetValue("time", out var time))
         {
             this.leaderboardEntries.Time = Convert.ToDouble(time);
         }
-    }
-
-    protected override async Task OnInitializedAsync()
-    {
-        await base.OnInitializedAsync();
-
-        var textId = await GetSelectedText();
-
-        if (textId == -1)
+        else
         {
-            return;
+            this.parameterAreIncomplete = true;
         }
-
-        this.activeLeaderboardPath = $"leaderboard-text-{textId}.json";
     }
 
-    private async Task<int> GetSelectedText()
+    private async Task GetCurrentLeaderboardPath()
     {
         var filePath = Path.Combine(this.Env.WebRootPath, "data", "text-selection.json");
 
@@ -55,17 +60,17 @@ public partial class EnterLeaderboard
 
             var textBib = JsonConvert.DeserializeObject<List<TextBib>>(json);
 
-            return textBib!.Single(e => e.Aktiviert).Id;
+            var textId = textBib!.Single(e => e.Aktiviert).Id;
+
+            this.activeLeaderboardPath = $"leaderboard-text-{textId}.json";
         }
         catch (FileNotFoundException)
         {
             this.Logger.LogError($"Leaderboard.json wurde nicht gefunden. Verwendeter Pfad: {filePath}");
-            return -1;
         }
         catch (Exception ex)
         {
             this.Logger.LogError(ex, $"Exception occured. ExceptionMessage: {ex.Message}");
-            return -1;
         }
     }
 
